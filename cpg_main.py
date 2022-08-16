99ec8b4ae32a773f5e84ac5dd260d9dcaca28464
justKleb <74954828+justKleb@users.noreply.github.com>
2022-08-16 12:49:08 +0300

    🥇First Commit

---
+++ cpg_main.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+window = tk.Tk()
+window.title("Customizable Password Gen")
+window.geometry("1080x720")
+window.resizable(False, False)
+window.configure(background='#F7ECDE')
+ttl_lbl = tk.Label()
+
+tk.mainloop()
