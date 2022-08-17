3e018e808fb288a358ceae3c46271433c799574e
justKleb <74954828+justKleb@users.noreply.github.com>
2022-08-17 22:29:16 +0300

    🤏Add code execution time output

--- cpg_main.py
+++ cpg_main.py
@@ -1,11 +1,9 @@
 #import utils.cpg_utils as cpgu
+import time
+
 from utils.cpg_utils import passwordGen as pG
 from utils.cpg_utils import utils as ut
-neg = pG.genInBase64(19)
-#x, y = pG.genViaMouse()
+start = time.time()
+neg = pG.genInBase64(10000)
 print(neg)
-#print(len(cpgu.inBase64(19)))
-print(pG.mirror(neg))
-print(pG.genRandomLetters(10))
-print(pG.genRandomNums(10))
-print(pG.genViaMouse(10))
+print("took %s seconds" % (time.time() - start))
