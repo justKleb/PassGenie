1992db7c0883002e47e0f26771ae65e5ce848403
justKleb <74954828+justKleb@users.noreply.github.com>
2022-08-23 18:35:16 +0300

    📦Add basic config

--- cpg_main.py
+++ cpg_main.py
@@ -1,4 +1,4 @@
-import os
+import json
 import time
 from utils.cpg_utils import passwordGen as pG
 from utils.cpg_utils import utils as ut
@@ -8,11 +8,23 @@ from utils.cpg_utils import crypting as crypt
 from utils.cpg_utils import log
 from cryptography.fernet import Fernet as fnet
 
+log.clearLog()
+
 start = time.time()
 
-log.clearLog()
-crypt.keyGen()
+cfg = []
+def cfgGet():
+    settingsFile = open('settings.json')
+    settings = json.load(settingsFile)
+    settingsFile.close()
+    toLogSETTINGS = []
+    for i in settings["Settings"]:
+        toLogSETTINGS.append(f'{i} is set to {settings["Settings"][i]}')
+        cfg.append(settings["Settings"][i])
+        log.toLog(f"Settings: {', '.join(toLogSETTINGS)}")
 
+cfgGet()
 
-
-print("Run took %s seconds" % (time.time() - start))
+if cfg[1]:
+    log.toLog("Run took %s seconds" % (time.time() - start))
+    print("Run took %s seconds" % (time.time() - start))
